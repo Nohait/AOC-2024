26be1911ac11f5c126e96b0a8deb712127b3a9d7
Nohait <noe.contraires@hotmail.fr>
2024-12-13 15:11:08 +0100

    com

---
+++ day13pb1.py
@@ -0,0 +1,9 @@
+class Machine :
+    def __init__(self,ax,ay,bx,by,px,py) :
+        self.prize = px,py
+        self.ax = ax
+        self.ay = ay
+        self.bx = bx
+        self.by = by
+        self.cost = 0
+        
